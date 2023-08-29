7b747730b0705cc8ff2df5f7afc59aa663bd71f7
Shailendra Kumar <fullstacksk@gmail.com>
2023-08-29 12:12:34 +0530

    first use of useQuery

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import { useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+
+const posts = [
+  {id:1,title:"titile 1"},
+  {id:2,title:"titile 2"},
+]
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const postQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => wait(1000).then(()=> [...posts]),
+    // queryFn: () => Promise.reject("Error Message"),
+    
+  })
 
+  if (postQuery.isLoading)
+    return <h1>Loading...</h1>
+  
+  if (postQuery.isError)
+    return <pre>{JSON.stringify(postQuery.error)}</pre>
   return (
     <>
-      <h1>TanStack Query [React Query]</h1>
-      <h3>Count : {count}</h3>
-      <button onClick={()=>setCount(count + 1)}>Increment</button>
+      <h1>TanStack Query</h1>
     </>
   )
+}
+function wait(duration) {
+  return new Promise(resolve => setTimeout(resolve, duration))
 }
 
 export default App
