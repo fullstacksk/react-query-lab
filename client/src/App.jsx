e81369f6b699723002ddb10513f04935329bce4b
Shailendra Kumar <fullstacksk@gmail.com>
2023-08-29 18:16:17 +0530

    added PostListPaginated and PostListInfinite comps

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import PostsList1 from "./PostList1"
 import PostsList2 from "./PostList2"
+import { PostListPaginated } from "./PostListPaginated"
+import { PostListInfinite } from "./PostListInfinite"
 
 export default function App() {
   const [currentPage, setCurrentPage] = useState(<PostsList1 />)
@@ -13,6 +15,13 @@ export default function App() {
       <button onClick={() => setCurrentPage(<PostsList2 />)}>
         Posts List 2
       </button>
+      <button onClick={() => setCurrentPage(<PostListPaginated />)}>
+        Post List Paginated
+      </button>
+      <button onClick={() => setCurrentPage(<PostListInfinite />)}>
+        Post List Infinite
+      </button>
+      <br />
       {currentPage}
     </div>
   )
